cb66b6f3b68c31a868e731b5a33d3afbbc8750be
Sujit <mnamesujit@gmail.com>
2023-07-17 11:34:07 +0530

    Insider Navbar Communication section section work done

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar/Navbar'
 function App() {
 
   return (
-    <div className="main">
+    <div className="navbar">
       <Navbar />
     </div>
   )
--- src/components/Navbar/DesktopLayout.tsx
+++ src/components/Navbar/DesktopLayout.tsx
@@ -1,8 +1,28 @@
-
+import { BiLogoTwitter, BiLogoInstagramAlt, BiLogoLinkedinSquare } from 'react-icons/bi';
+import {MdEmail, MdLocationOn} from 'react-icons/md'
 const DesktopLayout = () => {
   return (
       <div>
-          <h1>naVigation Rendered</h1>
+      <div className="social">
+        <div className="communication">
+          <div>
+            <i><MdLocationOn /></i>
+            <p>Saket, New Delhi, 11017</p>
+          </div>
+          <div>
+            <i><MdEmail /></i>
+            <p> <a href="mail-to:query@safeyourweb.com">query@safeyourweb.com</a></p>
+          </div>
+        </div>    
+        <div className="social-links">
+          <i><a href="https://twitter.com/safeyourweb"><BiLogoTwitter /></a></i>
+          <i><a href="https://instagram.com/safeyourweb?igshid=YmMyMTA2M2Y="><BiLogoInstagramAlt /></a></i>
+          <i><a href="https://www.linkedin.com/company/safe-your-web/"><BiLogoLinkedinSquare /></a></i>
+          </div>
+       </div>
+       <div className="navigation">
+        
+       </div>
       </div>
   )
 }
